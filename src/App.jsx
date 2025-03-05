214ffb5782189c9c1874af1eee87fdf26546ccc3
Karin Ljunggren <kaljunggren@gmail.com>
2025-03-05 19:24:13 +0100

    404 index

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes , Navigate} from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ThemeProvider from "./context/ThemeContext";
 function App() {
-  
   return (
     <BrowserRouter>
       <ThemeProvider>
-          <Header />
+        <Header />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="*" element={<NotFound/>} /> */}
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<Navigate to="/404.html" />} />
+        </Routes>
 
-          <Footer />
+        <Footer />
       </ThemeProvider>
     </BrowserRouter>
   );
